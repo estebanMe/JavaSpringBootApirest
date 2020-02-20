43a64fd812fa3027bcdb861b28c4ca2922ba8396
victoresteban.mercado <victoresteban.mercado@bbva.com>
2020-02-20 17:16:54 -0300

    first commit

---
+++ src/main/java/com/moken/springboot/backend/apirest/models/services/IClienteService.java
@@ -0,0 +1,15 @@
+package com.moken.springboot.backend.apirest.models.services;
+
+import java.util.List;
+
+import com.moken.springboot.backend.apirest.models.entity.Cliente;
+
+public interface IClienteService {
+     public List<Cliente>findAll();
+     
+     public Cliente findById(Long id);
+     
+     public Cliente save(Cliente cliente);
+     
+     public void delete(Long id);
+}
